277684e041d97524cf8abc1528335c20eea4a3bd
Anvesh Jain P <quic_ajainp@quicinc.com>
2025-06-26 16:08:22 +0530

    platform-kernel: fastrpc: build only required fastrpc driver
    build fastrpc drivers based on requirment and add symbol dependancy
    from rsm_fe driver.
    
    Signed-off-by: Anvesh Jain P <quic_ajainp@quicinc.com>
    Change-Id: I6b0908a41b8ba521ad2d496d1df49e41094294e6

--- drivers/virtual_fastrpc/Android.mk
+++ drivers/virtual_fastrpc/Android.mk
@@ -1,5 +1,6 @@
 VIRTIO_FASTRPC_SELECT := CONFIG_VIRTIO_FASTRPC=m
 HYBRID_FASTRPC_SELECT := CONFIG_HYBRID_FASTRPC=m
+HYBRID_FASTRPC_RSM_SELECT := CONFIG_HYBRID_FASTRPC_RSM=y
 
 DLKM_DIR := $(TOP)/device/qcom/common/dlkm
 LOCAL_PATH := $(call my-dir)
@@ -7,10 +8,10 @@ LOCAL_PATH := $(call my-dir)
 VRPC_BLD_DIR := $(abspath .)/vendor/qcom/opensource/platform-kernel/drivers/virtual_fastrpc
 
 KBUILD_OPTIONS += VRPC_ROOT=$(VRPC_BLD_DIR)
-KBUILD_OPTIONS += $(HYBRID_FASTRPC_SELECT)
+
+ifeq ($(TARGET_HAS_VIRTIO_FASTRPC), true)
 KBUILD_OPTIONS += $(VIRTIO_FASTRPC_SELECT)
 KBUILD_OPTIONS += BOARD_PLATFORM=$(TARGET_BOARD_PLATFORM)
-
 # Virtual fastrpc
 ###########################################################
 include $(CLEAR_VARS)
@@ -19,12 +20,26 @@ LOCAL_MODULE      := vfastrpc.ko
 LOCAL_MODULE_KBUILD_NAME := vfastrpc.ko
 LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
 include $(DLKM_DIR)/Build_external_kernelmodule.mk
+endif
 
+ifeq ($(TARGET_HAS_HYBRID_FASTRPC), true)
+KBUILD_OPTIONS += $(HYBRID_FASTRPC_SELECT)
+KBUILD_OPTIONS += BOARD_PLATFORM=$(TARGET_BOARD_PLATFORM)
 # Hybrid fastrpc
 ###########################################################
 include $(CLEAR_VARS)
+
+ifeq ($(TARGET_HAS_HYBRID_FASTRPC_RSM), true)
+KBUILD_OPTIONS += KBUILD_EXTRA_SYMBOLS+=$(PWD)/$(call intermediates-dir-for,DLKM,rsm-fe-module-symvers)/Module.symvers
+
+LOCAL_REQUIRED_MODULES := rsm-fe-module-symvers
+LOCAL_ADDITIONAL_DEPENDENCIES += $(call intermediates-dir-for,DLKM,rsm-fe-module-symvers)/Module.symvers
+KBUILD_OPTIONS += $(HYBRID_FASTRPC_RSM_SELECT)
+endif
+
 LOCAL_SRC_FILES   := $(wildcard $(LOCAL_PATH)/**/*) $(wildcard $(LOCAL_PATH)/*)
 LOCAL_MODULE      := hfastrpc.ko
 LOCAL_MODULE_KBUILD_NAME := hfastrpc.ko
 LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
 include $(DLKM_DIR)/Build_external_kernelmodule.mk
+endif
